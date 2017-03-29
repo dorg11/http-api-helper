e6e78a963809ca659a9e5791a32210941a5a8303
Dor Galam <dorg@wix.com>
2017-03-29 10:51:38 +0300

    getting rid of react warnings for prod

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,11 @@ module.exports = {
     },
     plugins: [
       new webpack.optimize.OccurrenceOrderPlugin(),
+      new webpack.DefinePlugin({
+        'process.env': {
+          NODE_ENV: JSON.stringify('production')
+        }
+      }),
       new webpack.optimize.UglifyJsPlugin({
         compressor: {
           warnings: false
